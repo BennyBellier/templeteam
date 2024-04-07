8499dfe5b48834ca25278ebcd6dfeb0716e7b6c4
Benny Bellier <benny.bellier@gmail.com>
2024-04-07 20:53:40 +0200

    Replacing error by global-error ADding return to home to not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,31 +1,11 @@
-"use client"; // Error components must be Client Components
+import Link from 'next/link'
 
-import { Typography } from "@/components/ui/typography";
-import { useEffect } from "react";
-
-export default function Error({
-  error,
-  _reset,
-}: {
-  error: Error & { digest?: string };
-  _reset: () => void;
-}) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error("Error page: ", error.message);
-  }, [error]);
-
+export default function NotFound() {
   return (
-    <>
-      <header className="flex w-full flex-col items-center gap-10 border-b border-border px-1050 pb-16 md:flex-1">
-        <Typography as="h1" variant="title">
-          Erreur 404
-        </Typography>
-        <Typography as="h2" variant="description" className="w-3/4">
-          {error.message}
-        </Typography>
-      </header>
-      <div className="flex-6"></div>
-    </>
-  );
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
 }
